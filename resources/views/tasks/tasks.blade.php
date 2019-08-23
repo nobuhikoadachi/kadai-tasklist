5d84b3b93a2811d65534cf3807d707e44f51dd2b
kamekame <kamekame@gmail.com>
2019-08-23 21:44:54 +0000

    add column

--- resources/views/tasks/tasks.blade.php
+++ resources/views/tasks/tasks.blade.php
@@ -9,13 +9,15 @@
             <thead>
                 <tr>
                     <th>id</th>
+                    <th>ステータス</th>
                     <th>タスク</th>
                 </tr>
             </thead>
             <tbody>
                 @foreach ($tasks as $task)
                 <tr>
-                    <td>{{ $task->id }}</td>
+                    <td>{!! link_to_route('tasks.show', $task->id, ['id' => $task->id]) !!}</td>
+                    <td>{{ $task->status }}</td>
                     <td>{{ $task->content }}</td>
                 </tr>
                 @endforeach
@@ -24,10 +26,5 @@
     @endif
     
     {!! link_to_route('messages.create', '新規タスクの投稿', [], ['class' => 'btn btn-primary']) !!}
-    @foreach ($tasks as $task)
-    <tr>
-        <td>{!! link_to_route('tasks.show', $task->id, ['id' => $task->id]) !!}</td>
-        <td>{{ $task->content }}</td>
-    </tr>
-    @endforeach
+   
 @endsection
